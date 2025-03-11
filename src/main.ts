36d942b9a5b1f081416c06bdfc24927c9f0f66e3
Rodrigo Alvarez <junior@yetox.com>
2025-03-10 21:35:36 -0600

    habilitando options

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:5173',
     credentials: true,
-    methods: 'GET,POST',
+    methods: 'GET,POST,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
   await app.listen(process.env.PORT ?? 3000);
